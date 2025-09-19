292c72b5dab5c79a603448453e099ef173fcf858
Dsrrezende <douglas.dsrrezende@gmail.com>
2025-09-19 00:54:55 -0300

    arquivos java

---
+++ 3- Java/Meus codigos java/Soma.java
@@ -0,0 +1,8 @@
+public class Soma {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int soma = a + b;
+        System.out.println("A soma é: " + soma);
+    }
+}
