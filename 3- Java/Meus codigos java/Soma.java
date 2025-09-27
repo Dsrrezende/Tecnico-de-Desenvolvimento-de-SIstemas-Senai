d214255195929f171676c1b73baafcb454010408
Dsrrezende <douglas.dsrrezende@gmail.com>
2025-09-27 14:21:54 -0300

    Folder Meus codigos java Deleted

--- 3- Java/Meus codigos java/Soma.java
+++
@@ -1,8 +0,0 @@
-public class Soma {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 7;
-        int soma = a + b;
-        System.out.println("A soma é: " + soma);
-    }
-}
